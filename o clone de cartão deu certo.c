ed43a03ffa0233b0dd60a014ad2a60305741f59e
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-10-12 20:47:48 -0300

    clonecartao2.1.c
    eu desisto, tinha um zero a menos, hahahahahahahahaha eu amo me sentir um cego hahahahaha vontade de quebrar uma parede com a cabeça

--- o clone de cartão deu certo.c
+++ o clone de cartão deu certo.c
@@ -4,11 +4,15 @@ int main()
 	int total_ciclo1 = 0;
 	unsigned long long int cartao = 403600000000014;
 	do
-	{
-		cartao /= 10;
-		total_ciclo1 += (cartao%10)*2;
-		printf("t%d\n", total_ciclo1);
-		cartao /= 10;
-	}while(cartao > 0);
+{
+	cartao /= 10;
+	ultimo_numero = (cartao%10)*2;
+	if(ultimo_numero < 10){
+	total_ciclo1 += ultimo_numero;
+	}else{
+	total_ciclo1 += ((ultimo_numero%10) + (ultimo_numero/10));
+	}
 	printf("t%d\n", total_ciclo1);
+	cartao /= 10;
+}while(cartao > 0);
 }
