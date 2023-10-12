652be5bafd5356d4139d0b8aafb3bad458b1c3d2
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-10-12 20:53:50 -0300

    clonecartao3.0.c
    feito, agora se pode ter certeza que o clone do cartão tem os números certos

--- o clone de cartão deu certo.c
+++ o clone de cartão deu certo.c
@@ -1,18 +1,31 @@
 # include<stdio.h>
 int main()
 {
-	int total_ciclo1 = 0;
-	unsigned long long int cartao = 403600000000014;
+	int ultimo_numero, total_ciclo1 = 0, total_ciclo2 = 0;
+	unsigned long long int cartao = 4003600000000014;
 	do
-{
-	cartao /= 10;
-	ultimo_numero = (cartao%10)*2;
-	if(ultimo_numero < 10){
-	total_ciclo1 += ultimo_numero;
+	{
+		cartao /= 10;
+		ultimo_numero = (cartao%10)*2;
+		if(ultimo_numero < 10){
+		total_ciclo1 += ultimo_numero;
+		}else{
+		total_ciclo1 += ((ultimo_numero%10) + (ultimo_numero/10));
+		}
+		cartao /= 10;
+	}while(cartao > 0);
+	
+	cartao = 4003600000000014;
+	do
+	{
+		ultimo_numero = cartao%10;
+		total_ciclo2 += ultimo_numero;
+		cartao /= 100;
+	}while(cartao > 0);
+	
+	if((total_ciclo1 + total_ciclo2)%10 == 0){
+		printf("valido, pois a soma foi %d ", (total_ciclo1 + total_ciclo2));
 	}else{
-	total_ciclo1 += ((ultimo_numero%10) + (ultimo_numero/10));
+		printf("valido, pois a soma foi %d ", (total_ciclo1 + total_ciclo2));
 	}
-	printf("t%d\n", total_ciclo1);
-	cartao /= 10;
-}while(cartao > 0);
 }
